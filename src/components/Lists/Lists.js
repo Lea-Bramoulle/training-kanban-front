227aa6829c61bfb24b7d6f1a5ea086309d202322
Lea-Bramoulle <l.bramoulle@hotmail.fr>
2022-10-29 12:32:32 +0200

    :rocket: feat : display lists and tasks on board

--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -15,7 +15,34 @@ function Lists() {
 
   console.log(listsOfBoardData);
 
-  return <div className="lists-container">coucou lists</div>;
+  return (
+    <div className="lists-container">
+      {listsOfBoardData?.map((element) => (
+        <div className="list">
+          <div className="list-header">
+            <h2 className="list-title">
+              <div
+                className="list-title-bullet"
+                style={{ backgroundColor: `#${element.color}` }}
+              ></div>
+              {element.name} ( {element.tasks.length} )
+            </h2>
+          </div>
+          <div className="tasks">
+            {element.tasks.map((element) => (
+              <div className="task-container">
+                <p className="task-title">{element.name}</p>
+                <p className="task-subtitle">
+                  {element.subtasks.filter((e) => e.is_done === true).length} of{' '}
+                  {element.subtasks.length} subtasks
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Lists;
